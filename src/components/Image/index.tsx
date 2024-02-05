f31b8229608f0805a03080ed0890ee13535a69dd
Abhishek Saxena <saxenaa218@gmail.com>
2024-02-06 01:38:13 +0530

    add: fallback url for image

---
+++ src/components/Image/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState, HTMLProps } from "react";
+
+interface ImageWrapperProps extends HTMLProps<HTMLImageElement> {
+  src: string;
+  alt: string;
+  fallbackSrc: string;
+}
+
+const ImageWrapper: React.FC<ImageWrapperProps> = ({
+  src,
+  alt,
+  fallbackSrc,
+  ...props
+}) => {
+  const [error, setError] = useState(false);
+
+  const handleImageError = () => {
+    setError(true);
+  };
+
+  return (
+    <img
+      src={error ? fallbackSrc : src}
+      alt={alt}
+      onError={handleImageError}
+      loading="lazy"
+      {...props}
+    />
+  );
+};
+
+export default ImageWrapper;
--- src/components/Tile/index.tsx
+++ src/components/Tile/index.tsx
@@ -2,12 +2,20 @@ import { FC } from "react";
 
 import { BASE_URL } from "@common";
 import { Tile } from "src/api/types";
+import ImageWrapper from "@components/Image";
 
 const Tile: FC<Tile> = ({ "poster-image": posterImage, name }) => {
   const imageUrl = `${BASE_URL}/images/${posterImage}`;
+  const fallbackImageUrl =
+    BASE_URL + "/images/placeholder_for_missing_posters.png";
   return (
     <>
-      <img src={imageUrl} alt={name} loading="lazy" className="w-full" />
+      <ImageWrapper
+        src={imageUrl}
+        alt={name}
+        className="w-full"
+        fallbackSrc={fallbackImageUrl}
+      />
       <p className="mt-1">{name}</p>
     </>
   );
